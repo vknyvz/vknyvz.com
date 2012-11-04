11edf63fb9ca45438b5bdcfaf90408a0d2a0444f
Volkan Yavuz <vkn@vknyvz.com>
2012-11-04 15:43:33 -0500

    dynamic app_env

--- public/index.php
+++ public/index.php
@@ -7,5 +7,15 @@ set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../
    
 require_once 'Zend/Application.php';
 
+switch(APPLICATION_ENV) {
+	case 'development':
+		define('SERVER_PATH', '/var/www/vkn.com');
+		break;	
+	case 'production':
+	case '':		
+		define('SERVER_PATH', '/home2/volkanan/www/vknyvz');
+		break;	
+}
+
 $application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/config.xml');
 $application->bootstrap()->run();
